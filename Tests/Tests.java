9e95819d58a4e3ee3f608d86df36b577d2f12238
91086831 <am2662004@gmail.com>
2022-04-08 12:52:38 -0700

    Duplicated the RegEx Student template

--- Tests/Tests.java
+++ Tests/Tests.java
@@ -1,2 +1,82 @@
-public class Tests {
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegexTest {
+
+    @Test
+    void FirstNameLetterACapTrue()
+    {
+        RegExStudent tester1 = new RegExStudent();
+        tester1.setFirstName("A");
+        assertTrue(tester1.getFirstName().matches("[A-Z]"));
+    }
+
+    @Test
+    void FirstNameLetterBCapTrue()
+    {
+        RegExStudent tester1 = new RegExStudent();
+        tester1.setFirstName("B");
+        assertTrue(tester1.getFirstName().matches("[A-Z]"));
+    }
+
+    @Test
+    void FNLetterBMoreFalse()
+    {
+        RegExStudent tester1 = new RegExStudent();
+        tester1.setFirstName("Bob");
+        assertFalse(tester1.getFirstName().matches("[A-Z]"));
+    }
+
+    @Test
+    void FNLetterbFalse()
+    {
+        RegExStudent tester1 = new RegExStudent();
+        tester1.setFirstName("b");
+        assertFalse(tester1.getFirstName().matches("[A-Z]"));
+    }
+
+    @Test
+    void FNLetterBoTrue()
+    {
+        RegExStudent tester1 = new RegExStudent();
+        tester1.setFirstName("Bo");
+        assertTrue(tester1.getFirstName().matches("[A-Z][a-z]"));
+    }
+
+    @Test
+    void FNLetterBFalse()
+    {
+        RegExStudent tester1 = new RegExStudent();
+        tester1.setFirstName("B");
+        assertFalse(tester1.getFirstName().matches("[A-Z][a-z]"));
+    }
+
+    @Test
+    void FNLetterBLengthTrue()
+    {
+        RegExStudent tester1 = new RegExStudent();
+        tester1.setFirstName("Bfwrgertbgdcsahe");
+        assertTrue(tester1.getFirstName().matches("[A-Z][a-z]*"));
+    }
+
+    @Test
+    void FNLWithHyphenTrue()
+    {
+        RegExStudent tester1 = new RegExStudent();
+        tester1.setFirstName("John-bob");
+        assertTrue(tester1.getFirstName().matches("[A-Z]+([-][a-z])*"));
+    }
+
+    @Test
+    void getFirstName() {
+    }
+
+    @Test
+    void setFirstName() {
+    }
+
+    @Test
+    void testToString() {
+    }
 }
--- src/Regex.java
+++ src/Regex.java
@@ -1,2 +1,50 @@
-public class Regex {
+import java.util.Scanner;
+
+public class Regex
+{
+    Scanner input = new Scanner(System.in);
+    private String firstName;
+    private String lastName;
+    private int age;
+    private boolean isActive;
+    private int stuID;
+
+    public Regex()
+    {
+        firstName = "";
+        lastName = "";
+        age = 0;
+        isActive = false;
+        stuID = 00000;
+    }//end of public default constructor
+
+    public String getFirstName()
+    {
+        return firstName;
+    }//firstName getter
+
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }//firstName setter
+
+    private String validateFirstName(String firstName)
+    {
+        while(!firstName.matches("[A-Z][a-zA-Z]*"))
+        {
+            System.out.println("Format Incorrect");
+            firstName = input.nextLine();
+        }
+        return firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "RegExStudent{" +
+                "firstName = " + firstName + '\'' +
+                ", lastName = " + lastName + '\'' +
+                ", age = " + age + '\'' +
+                ", isActive = " + isActive + '\'' +
+                ", stuID = " + stuID;
+    }
 }
